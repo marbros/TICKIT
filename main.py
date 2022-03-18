11a39d4e7d3c0a5adc7d0f38c3df7cecc7d2d8b6
marbros <enigmail333@yahoo.es>
2022-03-18 08:31:46 -0500

    fixed save and model prediction, values negatives are omited

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import os,sys
 sys.modules['sklearn.externals.joblib'] = joblib
 
 # Models
-sell_model = open("models/tickit.pkl", "rb")
-sell_clf = joblib.load(sell_model)
+# sell_model = open("models/tickit.pkl", "rb")
+# sell_clf = joblib.load(sell_model)
 
 #init app
 app = FastAPI()
@@ -26,15 +26,15 @@ async def items(date):
 
 #ML Aspect
 
-@app.get('/predict/{date}')
-async def predict(date):
-  prediction = sell_clf.predict(date)
-  if prediction[0] == 0:
-    result = "something"
-  else:
-    result = "F"
+# @app.get('/predict/{date}')
+# async def predict(date):
+#   prediction = sell_clf.predict(date)
+#   if prediction[0] == 0:
+#     result = "something"
+#   else:
+#     result = "F"
 
-  return  {"orig_date": date, "prediction": result}
+#   return  {"orig_date": date, "prediction": result}
 
 if __name__ == '__main__':
   uvicorn.run(app,host="127.0.0.1", port=8000)
